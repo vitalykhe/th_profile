87ce0e3fbe9a567e77eecb3d1af035ec4b93bce3
Vit K <vasya.krakovsky@gmail.com>
2019-03-19 11:17:34 +0400

    2

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ http.createServer((request, response) => {
 }).listen(1337, '127.0.0.1');
 
 console.log('Server is running at http://127.0.0.1:1337');
-console.log('Server is running at http://127.0.0.1:1337');
 
 
 
