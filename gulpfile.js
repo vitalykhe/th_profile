f123e38102f42e4b58370a18294c9ed36727b40a
Vit K <vasya.krakovsky@gmail.com>
2019-03-19 12:22:26 +0400

    igronoring something

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+cont gulp = require('gulp');
+
+//gulp top level functions
+
+//gulp.task  - define tasks
+//gulp.src = point files to use
+//gulp.dest = destination to output
+//gulp.watch = watch files and folders for changes
+
+gulp.task('message', () => {
+  return console.log('gulp is running..');
+});
